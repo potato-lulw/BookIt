fca72e2e2a5a5d0608b9544a572662170304d8d9
potato-lulw <om.patil17072@gmail.com>
2025-10-31 01:29:20 +0530

    client: Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline"><Button>Hello, World</Button></h1>
+    </>
+  )
+}
+
+
+
+export default App
